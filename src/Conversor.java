7d904be5522209752419004382cca4247defc855
Samantha Hernandez <samhercab@gmail.com>
2025-01-09 23:14:40 -0600

    fin

---
+++ src/Conversor.java
@@ -0,0 +1,26 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conversor {
+    public Divisas conversor(String original, String convertir, double cantidad){
+        var key= "aa47ceecea6eb2589a0f2391";
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/"+key+"/pair/"+original+"/"+convertir+"/"+cantidad);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Divisas.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa divisa, por favor asegurate de estar usando el código correcto");
+        }
+    }
+
+}
---
+++ src/Divisas.java
@@ -0,0 +1,4 @@
+public record Divisas(double conversion_rate,
+                      double conversion_result,
+                      String time_next_update_utc) {
+}
